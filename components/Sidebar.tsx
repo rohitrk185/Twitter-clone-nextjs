8bbf43199a83ea312fc88c2e85299e92a3f5928d
RohitKumar *2102 <rohitrk185>
2022-09-22 16:33:58 +0530

    set-up twitter login option

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,18 +10,33 @@ import {
     HomeIcon
 } from '@heroicons/react/outline'
 import SidebarRow from './SidebarRow'
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 
 function Sidebar() {
+  const {data: session} = useSession();
   return (
     <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
         <img src="https://links.papareact.com/drq" alt="twitter-icon" className='h-10 w-10 m-3'/>
-        <SidebarRow Icon={HomeIcon} title="Home"/>
-        <SidebarRow Icon={HashtagIcon} title="Explore"/>
-        <SidebarRow Icon={BellIcon} title="Notifications"/>
-        <SidebarRow Icon={MailIcon} title="Messages"/>
-        <SidebarRow Icon={BookmarkIcon} title="Bookmarks"/>
-        <SidebarRow Icon={UserIcon} title="Sign In"/>
-
+        <SidebarRow 
+          Icon = {HomeIcon} 
+          title="Home"/>
+        <SidebarRow 
+          Icon = {HashtagIcon}
+          title="Explore"/>
+        <SidebarRow 
+          Icon = {BellIcon}
+          title="Notifications"/>
+        <SidebarRow 
+          Icon = {MailIcon} 
+          title="Messages"/>
+        <SidebarRow 
+          Icon = {BookmarkIcon} 
+          title="Bookmarks"/>
+        <SidebarRow 
+          Icon = {UserIcon} 
+          title = {session ? 'Sign Out' : 'Sign In'}
+          onClick = {session ? signOut : signIn}/>
         <SidebarRow Icon={DotsCircleHorizontalIcon} title="More"/>
     </div>
   )
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-
+import NextAuth from 'next-auth'
+import TwitterProvider from 'next-auth/providers/twitter'
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
