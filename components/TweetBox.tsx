ddc81841f6b22ece001bddeeecb74c84e415e467
RohitKumar *2102 <rohitrk185>
2022-09-30 14:31:38 +0530

    render twitter profile image of logged-in user and disable the button to tweet if the user hasn't logged-in

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -6,12 +6,17 @@ import {
   PhotographIcon,
   SearchCircleIcon
 } from '@heroicons/react/outline'
+import { useSession } from 'next-auth/react';
 
 function TweetBox() {
-  const [input, setInput] = useState<string>('');
+  	const [input, setInput] = useState<string>('');
+	const {data: session} = useSession();
+
+
   return (
     <div className='flex space-x-2 p-5'>
-        <img src="https://links.papareact.com/gll" alt="" 
+        <img alt=""
+		src={session?.user?.image || "https://links.papareact.com/gll"} 
         className='h-14 w-14 object-cover rounded-full mt-4
         '/>
         
@@ -31,7 +36,7 @@ function TweetBox() {
                 		<LocationMarkerIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-125'/>
               		</div>
               		<button type="submit" 
-					disabled={!input} 
+					disabled={!input || !session} 
 					className='bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40'>
 						Tweet
               		</button>
