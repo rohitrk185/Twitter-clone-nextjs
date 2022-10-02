e313581c9e3b740733477182f474ade09a055fdf
RohitKumar *2102 <rohitrk185>
2022-10-02 09:21:54 +0530

    final commit

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -1,4 +1,4 @@
-import  {Comment} from '../typings'
+import {Comment} from '../typings'
 
 export const fetchComments = async (tweetId: string) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getComments?tweetId=${tweetId}`);
